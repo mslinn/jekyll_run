ce2dc46fa10c463b333223f5980fc66c3e9f4649
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-18 10:25:48 -0400

    -

--- lib/jekyll_run.rb
+++ lib/jekyll_run.rb
@@ -2,21 +2,15 @@
 
 require "jekyll"
 require_relative "jekyll_run/version"
-require "jekyll_logger_factory"
+require "jekyll_plugin_logger"
 
 # Jekyll tag plugin that executes a program and returns the output from STDOUT.
 # Because the output includes the command that was executed,
 # and is contains unselectable span tags,
 # this plugin is intended to be embedded within a pre tag.
 module Jekyll
-  class Error < StandardError; end
-
   # Executes a program and returns the output from STDOUT.
   class RunTag < Liquid::Tag
-    class << self
-      attr_accessor :logger
-    end
-
     # Constructor.
     # @param tag_name [String] is the name of the tag, which we already know.
     # @param command_line [Hash, String, Liquid::Tag::Parser] the arguments from the web page.
@@ -26,18 +20,17 @@ module Jekyll
       super
       @command = command_line
       @command = "" if @command.nil? || @command.empty?
-      RunTag.logger = nil
     end
 
     # Method prescribed by the Jekyll plugin lifecycle.
     # @return [String]
-    def render(context)
-      RunTag.logger = Jekyll::LoggerFactory.new("run", context.config, :info)
-      RunTag.logger.info "Running #{@command}".green
+    def render(_context)
+      info "Running #{@command}"
       output = `#{@command}`.rstrip
       "<span class='unselectable'>$ </span>#{@command}\n<span class='unselectable'>#{output}</span>"
     end
   end
 end
 
-Liquid::Template.register_tag(Jekyll::RunTag.logger.progname, Runner::RunTag)
+Liquid::Template.register_tag("run", Jekyll::RunTag)
+Jekyll.info "Loaded run plugin."
